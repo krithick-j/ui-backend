05d7e2a34d2fa19cad3f47947516128a8d720e70
Sri <j.krithick@gmail.com>
2024-01-29 15:29:34 +0530

    product/filterByCategory api create

--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -16,3 +16,10 @@ func GetProductCategoriesController(c *fiber.Ctx) error {
 	res := service.GetProductCategories()
 	return c.Status(http.StatusOK).JSON(res)
 }
+
+func GetProductByCategoryID(c *fiber.Ctx) error {
+	category_id := c.Params("category_id")
+	res := service.GetProductByCategoryID(category_id)
+
+	return c.Status(http.StatusOK).JSON(res)
+}

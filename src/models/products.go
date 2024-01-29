ebacf9567aafaa4c02b6c153ae4817545379e318
Sri <j.krithick@gmail.com>
2024-01-29 13:45:04 +0530

    GetAllProductCategories api create

--- src/models/products.go
+++ src/models/products.go
@@ -6,12 +6,15 @@ import (
 
 type Product struct {
 	gorm.Model
-	Name          string
-	Quantity      int
-	ProductImages []ProductImage
-	ShipmentTime  string
-	Price         int
-	SandH         int
-	RSP           int
-	AddToCart     bool
+	Name              string
+	Quantity          int
+	ProductImages     []ProductImage
+	ShipmentTime      string
+	Price             int
+	SandH             int
+	RSP               int
+	BV                int
+	AddToCart         bool
+	ProductCategoryID int
+	ProductCategory   ProductCategory
 }

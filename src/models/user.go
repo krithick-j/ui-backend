2167619d9bee63cfb2d39d96e88a3cd4f2f0c16c
Jegatheesan <jega@jega.in>
2024-02-18 05:56:04 +0530

    User registration Integration done, recursive bug fix

--- src/models/user.go
+++ src/models/user.go
@@ -2,32 +2,6 @@ package models
 
 import "gorm.io/gorm"
 
-//	type User struct {
-//		ID int `json:"id"`
-//		Title string `json:"title"`
-//		LastName string `json:"last_name"`
-//		FirstName string `json:"first_name"`
-//		ChequeName string `json:"cheque_name"`
-//		DistributorID string `json:"distributor_id"`
-//		Address1 string `json:"address_1"`
-//		Address2 string `json:"address_2"`
-//		TownOrCity string `json:"town_or_city"`
-//		District string `json:"district"`
-//		StateOrProvince string `json:"state_or_province"`
-//		PostalOrZipCode int `json:"postal_or_zip_code"`
-//		HomePhoneNo string `json:"home_phone_no"`
-//		MobilePhoneNo string `json:"mobile_phone_no"`
-//		EmailAddress string `json:"email_address"`
-//		ValidIDType string `json:"valid_id_type"`
-//		ValidIDNo string `json:"valid_id_no"`
-//		Nationality string `json:"nationality"`
-//		Dob string `json:"dob"`
-//		NameOfBenificiaryOrNominee string `json:"name_of_benificiary_or_Nominee"`
-//		Relationship string `json:"relationship"`
-//		MothersFamilyName string `json:"mothers_family_name"`
-//		PanCard string `json:"pan_card"`
-//	}
-
 type User struct {
 	gorm.Model
 	DistribID       string `json:"distrib_id"`
@@ -56,6 +30,6 @@ type TrackingCenter struct {
 	LeftPlace      string `json:"left_place"`
 	RightDistribID string `json:"right_distrib_id"`
 	RightPlace     string `json:"right_place"`
-	CenterCode     string `json:"center_code"`
+	Place          string `json:"place"`
 	Bv             int    `json:"bv"`
 }

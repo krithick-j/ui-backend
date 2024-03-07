2c74622fc70d20a293feb4cf04e9686f08909e87
Harini Sundar <harinis72004@gmail.com>
2024-03-07 18:06:02 +0530

    direct_bv added

--- src/repositories/rspTransaction.go
+++ src/repositories/rspTransaction.go
@@ -25,3 +25,14 @@ func GetAllRspByDistribId(distrib_id string) ([]int, *gorm.DB) {
 	result := configs.DB.Model(models.RspTransaction{}).Select("rsp").Where("distrib_id= ?", distrib_id).Find(&rsp)
 	return rsp, result
 }
+func AddDirectBvTx83(distrib_id string, order_id string, total_bv int) {
+	tx := models.RspTransaction{
+		DistribId: distrib_id,
+		OrderId:   order_id,
+		DirectBv:  total_bv,
+	}
+	result := configs.DB.Create(&tx) //insert into rspTransaction
+	if result.Error != nil {
+		fmt.Printf("Error %v\n", result.Error.Error())
+	}
+}

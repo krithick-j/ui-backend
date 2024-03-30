e217940c2ae1aebd1892ad34e063a0317e0b57d7
rajbiker <athisayarajbiker@gmail.com>
2024-03-30 14:07:04 +0530

    draft

--- configs/dbConfig.go
+++ configs/dbConfig.go
@@ -15,7 +15,7 @@ import (
 var DB *gorm.DB
 
 func DbConnect(config *Config) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUserName, config.DBUserPassword, config.DBAddress, config.DBPort, config.DBName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUserName, config.DBUserPassword, config.DBHost, config.DBPort, config.DBName)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),

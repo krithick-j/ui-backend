8ae51ecebc97cba95fae2e38f98aaf01c37376d7
Jegatheesan Veeramalai <jegatheesan.veeramalai.contr@ironmountain.com>
2024-04-13 23:47:17 +0530

    cron updated

--- crons/rank_redeem_bv.py
+++ crons/rank_redeem_bv.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List
 
-from utils.config import DB
+from utils.config import DB, BV_VALUE
 
 def get_all_users_with_frequency() -> List[str]:
     '''
@@ -41,18 +41,56 @@ def alluser_upsert_rank() -> None:
     DB.commit()
     cursor.close()
 
+def process_ep(distrib_id: str, 
+               place: str) -> None:
+    print(f"Processing ev for {distrib_id} on {place}")
+    cursor = DB.cursor()
+    sqlInsert = f"""
+            INSERT INTO ep_transactions 
+              (distrib_id, reference, value) VALUES
+              ('{distrib_id}', '{place}', {BV_VALUE*2});
+        """
+    sqlDeduct = f"""
+            INSERT INTO bv_transactions (distrib_id, place, side, trans_type, bv_value) VALUES
+            ('{distrib_id}', '{place}', 'left', 'redeem', -{BV_VALUE}),
+            ('{distrib_id}', '{place}', 'right', 'redeem', -{BV_VALUE});
+        """
+    sqlUpdate = f"""
+            UPDATE cheque_frequencies 
+                SET frequency = frequency + 1 
+            WHERE distrib_id = '{distrib_id}'
+        """
+    # Insert into ep_transaction
+    cursor.execute(sqlInsert, multi=True)
+    # Deduct bv from tracking center
+    cursor.execute(sqlDeduct, multi=True)
+    # Reset the frequencies
+    cursor.execute(sqlUpdate, multi=True)
+
+    DB.commit()
+    cursor.close()
+
 def allusers_update_earning_point() -> None:
     cursor = DB.cursor()
     for user in users:
-        if user["frequency"] is None or user["frequency"] < 5:
-            print(f"Skipping earning point check for {user['id']}")
+        if user["frequency"] is None or (user["frequency"] % 5 != 0):
+            # Skipping earning point check for {user['id']}
             continue
-        print(f"further checking for {user['id']}")
-        for center_code in ['001', '002', '003']:
-            sql = f"""
-                SELECT left_point, right_point From tracking_centers where distrib_id = {user['id']} AND place = {center_code};
-            """
-            cursor.execute(sql)
+        sql = f"""
+            SELECT 
+              place,
+              SUM(IF(side='left', bv_value, 0)) as left_point,
+              SUM(IF(side='right', bv_value, 0)) as right_point
+            FROM bv_transactions 
+            WHERE distrib_id = '{user["id"]}'
+            GROUP BY place;
+        """
+        cursor.execute(sql)
+        rows  = cursor.fetchall()
+        for row in rows:
+            if row[1] >= BV_VALUE and row[2] >= BV_VALUE:
+                process_ep(user['id'], row[0])
+                break
         # if user.left < 4000 or user.rightbv < 4000:
             # continue
         # Deduct 400 from left and deduct 4000 from right
@@ -73,9 +111,9 @@ def main()->None:
     # Update the rank for all users
     # alluser_upsert_rank()
     # Check earning point on 6th check
-    # allusers_update_earning_point()
+    allusers_update_earning_point()
     # Close the db at the end
-    activate_bv()
+    # activate_bv()
     DB.close()
 
 #Keep the users in global scope to get repetive db hit

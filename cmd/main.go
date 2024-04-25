4335b6fe4727cc4d664390acdb6bbe406e423847
Jegatheesan <jega@jega.in>
2024-04-25 16:26:48 +0530

    email verify with mailtrap

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,7 @@ func main() {
 	api := app.Group("/api")
 	api.Post("/auth/login", controllers.Login)
 	api.Post("/auth/register", controllers.UserRegistration)
+	api.Post("/auth/emailcode", controllers.SendEmailCode)
 
 	app.Use(jwtware.New(jwtware.Config{
 		SigningKey: jwtware.SigningKey{Key: []byte("secret")},

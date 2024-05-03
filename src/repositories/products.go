9fc8b8758529ade731128bb7247d660591eb6cf6
Jegatheesan <jega@jega.in>
2024-05-04 01:30:15 +0530

    typo fixed

--- src/repositories/products.go
+++ src/repositories/products.go
@@ -22,18 +22,18 @@ func GetAllProductCategories(productCategories []models.ProductCategory) ([]mode
 }
 
 func GetAllProductByCategoryID(category_id string, product []models.Product) ([]models.Product, *gorm.DB) {
-	result := configs.DB.Preload("ProductImage").Find(&product, "product_category_id = ?", category_id)
+	result := configs.DB.Find(&product, "product_category_id = ?", category_id)
 	return product, result
 }
 
 func GetAllProductByCategoryIdAndProductType(category_id string, productType string) ([]models.Product, *gorm.DB) {
 	var product []models.Product
-	result := configs.DB.Preload("ProductImage").Find(&product, "product_category_id = ? AND product_type=?", category_id, productType)
+	result := configs.DB.Find(&product, "product_category_id = ? AND product_type=?", category_id, productType)
 	return product, result
 }
 
 func GetAllEpProductByCategoryID(category_id string, product []models.Product) ([]models.Product, *gorm.DB) {
-	result := configs.DB.Preload("ProductImage").Find(&product, "product_category_id = ? and ep IS NOT NULL", category_id)
+	result := configs.DB.Preload("ProductImages").Find(&product, "product_category_id = ? and ep IS NOT NULL", category_id)
 	return product, result
 }
 

8acd3ff66e4b92c7d9b20a370f085cf146a484e2
Sri <j.krithick@gmail.com>
2024-05-19 01:34:32 +0530

    Orders get validation added

--- src/models/orders.go
+++ src/models/orders.go
@@ -6,33 +6,39 @@ import (
 
 type OrdersHeader struct {
 	gorm.Model
-	DistribId      string
-	OrderId        string
-	SubTotal       float64
-	TotalSandH     float64
-	TotalAmount    float64
-	TotalQuantity  float64
-	TotalTypeValue float64
-	ProductType    string
-	ContactName    string
-	ContactEmail   string
-	Address        string
-	City           string
-	District       string
-	State          string
-	ZipCode        uint64
-	Country        string
-	HomePhoneNo    string
-	MobilePhoneNo  string
-	OrdersLiner    []OrdersLiner
+	DistribId          string
+	OrderId            string
+	DeliveryStatus     string
+	CourierName        string
+	ShipmentTrackingNo string
+	DeliveredAt        string
+	SubTotal           float64
+	TotalSandH         float64
+	TotalAmount        float64
+	TotalQuantity      float64
+	TotalTypeValue     float64
+	ProductType        string
+	ContactName        string
+	ContactEmail       string
+	Address            string
+	City               string
+	District           string
+	State              string
+	ZipCode            uint64
+	Country            string
+	HomePhoneNo        string
+	MobilePhoneNo      string
+	OrdersLiner        []OrdersLiner
 }
 
 type OrdersLiner struct {
 	gorm.Model
 	OrdersHeaderID uint
+	ProductID      uint
+	ProductImage   string
 	Name           string
 	Quantity       uint
-	UnitPrice      uint64
+	UnitPrice      float64
 	ProductType    string
 	TypeValue      float64
 	SubTotal       float64

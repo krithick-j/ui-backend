02263f2fadd71c6116bddcff00ed585123b3b03a
Jegatheesan <jega@jega.in>
2024-05-26 18:48:36 +0530

    zipcode to string

--- src/models/user.go
+++ src/models/user.go
@@ -14,7 +14,7 @@ type User struct {
 	District        string `json:"district"`
 	StateOrProvince string `json:"state_or_province"`
 	EmailAddress    string `json:"email_address"`
-	PinOrZipCode    uint64 `json:"pin_or_zip_code"`
+	PinOrZipCode    string `json:"pin_or_zip_code"`
 	Country         string `json:"country"`
 	HomePhoneNo     string `json:"home_phone_no"`
 	MobilePhoneNo   string `json:"mobile_phone_no"`

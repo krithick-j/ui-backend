1e0fe3045f247c8402d51b23d66d0e4c0e417c02
Harini Sundar <harinis72004@gmail.com>
2024-05-27 19:42:26 +0530

    logger added

--- src/controllers/iCouponController.go
+++ src/controllers/iCouponController.go
@@ -16,6 +16,7 @@ func CreateICoupon(c *fiber.Ctx) error {
 	adminName := c.Params("admin_name")
 	fmt.Println(adminName)
 	if err := c.BodyParser(&iCouponIn); err != nil {
+		configs.Log.Errorln("Error on parsing icouponIn from CreateICoupon controller fn",err.Error())
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
 	}
 	fmt.Println(iCouponIn)
@@ -24,6 +25,7 @@ func CreateICoupon(c *fiber.Ctx) error {
 	// If no error occurred, commit the transaction
 	if err := tx.Commit().Error; err != nil {
 		tx.Rollback()
+		configs.Log.Errorln("Error on committing function name",err.Error())
 		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
 
 	}
@@ -43,6 +45,7 @@ func ValidateICoupon(c *fiber.Ctx) error {
 	distribID := c.Params("distrib_id")
 
 	if err := c.BodyParser(&payload); err != nil {
+		configs.Log.Errorln("Error on parsing payload from ValidateICoupon controller fn",err.Error())
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
 	}
 
--- src/repositories/products.go
+++ src/repositories/products.go
@@ -98,10 +98,10 @@ func EditCartProducts(distrib_id string, product_id string, payload models.CartI
 	return payload, result
 }
 
-func EditProduct(distrib_id string, product_id string, payload models.Product) (models.Product, *gorm.DB) {
-	result := configs.DB.Model(models.Product{}).Where("distrib_id=? AND product_id=?", distrib_id, product_id).Updates(payload)
-	return payload, result
-}
+// func EditProduct(distrib_id string, product_id string, payload models.Product) (models.Product, *gorm.DB) {
+// 	result := configs.DB.Model(models.Product{}).Where("product_id=?", distrib_id, product_id).Updates(payload)
+// 	return payload, result
+// }
 
 func SaveProductImage(productImages []models.ProductImage) error {
 

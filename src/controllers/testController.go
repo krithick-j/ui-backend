bf0a2e10f49ca47a0c1bdb52cc92b83066ce36a5
Sri <j.krithick@gmail.com>
2024-07-06 15:57:06 +0530

    distributorAppform service complete

--- src/controllers/testController.go
+++ src/controllers/testController.go
@@ -6,10 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// func TestController(c *fiber.Ctx) error {
-
-// 	var payload dto.AddTc
-
 func TestController(c *fiber.Ctx) error {
 	distridId := c.Params("distribId") //string
 	res, status := service.Test(distridId)

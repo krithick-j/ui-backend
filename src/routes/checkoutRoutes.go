3f10bca2fb41d90c730fb32ab91ce527e4c108bc
Jegatheesan <jega@jega.in>
2024-08-20 14:41:38 +0530

    cpa values get endpoint create

--- src/routes/checkoutRoutes.go
+++ src/routes/checkoutRoutes.go
@@ -11,7 +11,7 @@ func CheckoutRouter(router fiber.Router) {
 	// router.Post("/available", controllers.IsCheckqueAvailable)
 	router.Post("/chequeCount", controllers.TotalChequeValueByDistribId)
 	router.Post("/checkFrequencyAmount", controllers.AddFrequencyAmount)
-	router.Post("/cpaValues", controllers.GetValuesForCpa)
+	router.Get("/cpaValues/:distrib_id", controllers.GetValuesForCpa)
 	router.Post("/takeCheque", controllers.TakeChequeByDistribId)
 	router.Put("/changePin", controllers.EditChequePin)
 	router.Post("/login", controllers.ChequeLogin)

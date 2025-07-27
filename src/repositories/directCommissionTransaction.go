bcc9fd05fcdbc946625239486906261b59fc9ea4
Krithick Jegatheesan <j.krithick@gmail.com>
2025-07-27 15:21:38 +0530

    expired dc transaction exclude

--- src/repositories/directCommissionTransaction.go
+++ src/repositories/directCommissionTransaction.go
@@ -27,13 +27,13 @@ func GetDirectCommissionActiveValueByDistribId(ref_distrib_id string, tx *gorm.D
 }
 
 func GetDirectCommissionAllValueByDistribId(ref_distrib_id string, tx *gorm.DB) (float64, error) {
-	var value float64
-	err :=
-		tx.
-			Model(&models.DirectCommissionTransaction{}).
-			Select("COALESCE(sum(value), 0)").
-			Where("ref_distrib_id =?", ref_distrib_id).
-			Take(&value).
-			Error
-	return value, err
+    var value float64
+    err :=
+        tx.
+            Model(&models.DirectCommissionTransaction{}).
+            Select("COALESCE(sum(value), 0)").
+            Where("ref_distrib_id = ? AND expiry_date > NOW()", ref_distrib_id).
+            Take(&value).
+            Error
+    return value, err
 }
